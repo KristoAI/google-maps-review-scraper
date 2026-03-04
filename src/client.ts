3cfd6eeff978f9d3abbe879d6ba6669d14faa336
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2026-03-04 05:51:24 +0530

    chore: Update Chrome version in User-Agent string.

--- src/client.ts
+++ src/client.ts
@@ -8,7 +8,7 @@ export const client = new Impit({
     cookieJar: cookieJar,
     browser: "chrome",
     headers: {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
         "Accept": "*/*",
         "Accept-Language": "en-US,en;q=0.9",
         "Sec-Fetch-Dest": "empty",

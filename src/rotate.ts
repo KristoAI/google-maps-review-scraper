be7a0ca953917dd341c456970acc63f3ae349df5
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2026-05-08 20:06:53 +0530

    chore: update user-agent strings

--- src/rotate.ts
+++ src/rotate.ts
@@ -22,7 +22,7 @@ function createRotateClient(cookies?: Record<string, string>) {
     cookieJar: cookieJar,
     browser: "chrome",
     headers: {
-      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36",
       "Accept": "*/*",
       "Accept-Language": "en-US,en;q=0.9",
       "Sec-Fetch-Dest": "empty",
